db0f0f672047b82f912a44e4d34e4c9256a736c1
Anis ur Rehman <anis@kinnov.com>
2022-10-24 18:34:29 +0500

    Test code cleanup

--- MoviesDemoTests/MoviesDemoTests.swift
+++ MoviesDemoTests/MoviesDemoTests.swift
@@ -1,47 +1,51 @@
 //
-//  MoviesDemoTests.swift
-//  MoviesDemoTests
+//  MoviesDemoTests2.swift
+//  MoviesDemoTests2
 //
-//  Created by Anis ur Rehman on 2/11/20.
-//  Copyright © 2020 Anis ur Rehman. All rights reserved.
+//  Created by Anis Rehman on 19/10/2022.
+//  Copyright © 2022 Anis ur Rehman. All rights reserved.
 //
 
 import XCTest
-@testable import MoviesDemo
 
-//class MoviesDemoTests: XCTestCase {
-//
-//    override func setUp() {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testFetchMovies() {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//        var result: [Movie]?
-//        let expectation = self.expectation(description: "Movies Fetched")
-//        
-//        let moviesService = MoviesService()
-//        moviesService.fetchMovies(.topRated) { movies, error in
-//            result = movies
-//            expectation.fulfill()
-//        }
-//        waitForExpectations(timeout: 5, handler: nil)
-//        XCTAssertNotNil(result)
-//        if let result {
-//            XCTAssertGreaterThan(result.count, 0)
-//        }
-//    }
-//
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-//
-//}
+final class MoviesDemoTests2: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testFetchMovies() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        var result: [Movie]?
+        let expectation = self.expectation(description: "Movies Fetched")
+
+        let moviesService = MoviesService()
+        moviesService.fetchMovies(.topRated) { movies, error in
+            result = movies
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+        XCTAssertNotNil(result)
+        if let result {
+            XCTAssertGreaterThan(result.count, 0)
+        }
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
