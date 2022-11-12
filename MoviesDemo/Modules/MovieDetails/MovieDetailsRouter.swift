1caaba9936126a455674de1b48db0d3aee235181
Anis ur Rehman <anis@kinnov.com>
2022-11-12 19:56:31 +0500

    MovieDetails router updated

--- MoviesDemo/Modules/MovieDetails/MovieDetailsRouter.swift
+++ MoviesDemo/Modules/MovieDetails/MovieDetailsRouter.swift
@@ -10,21 +10,4 @@
 import UIKit
 
 class MovieDetailsRouter: MovieDetailsRoutable {
-
-    weak var viewController: UIViewController?
-
-    static func createModule(with movie: Movie) -> UIViewController {
-        // Change to get view from storyboard if not using progammatic UI
-        let storyBoard = UIStoryboard(name: Storyboard.main.rawValue, bundle: nil)
-        let viewController = storyBoard.instantiateViewController(identifier: StoryboardID.movieDetailsViewController.rawValue) as! MovieDetailsViewController
-        let interactor = MovieDetailsInteractor()
-        let router = MovieDetailsRouter()
-        let presenter = MovieDetailsPresenter(interface: viewController, interactor: interactor, router: router, movie: movie)
-
-        viewController.presenter = presenter
-        interactor.presenter = presenter
-        router.viewController = viewController
-
-        return viewController
-    }
 }
--- MoviesDemo/Modules/Movies/MoviesRouter.swift
+++ MoviesDemo/Modules/Movies/MoviesRouter.swift
@@ -14,8 +14,26 @@ class MoviesRouter: MoviesRoutable {
     var navigationController: UINavigationController?
     
     func routeToMovieDetails(movie: Movie) {
-        let movieDetailsViewController = MovieDetailsRouter.createModule(with: movie)
-        movieDetailsViewController.title = "Movie Details"
-        navigationController?.pushViewController(movieDetailsViewController, animated: true)
+        let viewController = setupMovieDetails(movie)
+        
+        viewController.title = "Movie Details"
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+}
+
+//MARK: - Private Functions
+extension MoviesRouter {
+    private func setupMovieDetails(_ movie: Movie) -> MovieDetailsViewController {
+        let storyBoard = UIStoryboard(name: Storyboard.main.rawValue, bundle: nil)
+        let movieDetailsViewController = storyBoard.instantiateViewController(identifier: StoryboardID.movieDetailsViewController.rawValue) as! MovieDetailsViewController
+
+        let interactor = MovieDetailsInteractor()
+        let router = MovieDetailsRouter()
+        let presenter = MovieDetailsPresenter(interface: movieDetailsViewController, interactor: interactor, router: router, movie: movie)
+
+        movieDetailsViewController.presenter = presenter
+        interactor.presenter = presenter
+
+        return movieDetailsViewController;
     }
 }
